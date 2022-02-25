ebbeabeb7420f6f0a6a6cb6d8b6245c9ac1f11c4
Guy - Mangkorn Katawong <90173357+Guy-Mangkorn@users.noreply.github.com>
2022-02-25 19:28:54 +0700

    Update Manager.java

--- android/src/main/java/com/ovalmoney/fitness/manager/Manager.java
+++ android/src/main/java/com/ovalmoney/fitness/manager/Manager.java
@@ -483,14 +483,17 @@ public class Manager implements ActivityEventListener {
     }
 
     private void processHeartRate(DataSet dataSet, WritableArray map) {
-
+        int index = 0;
+        WritableMap heartRateMap = Arguments.createMap();
         for (DataPoint dp : dataSet.getDataPoints()) {
             for(Field field : dp.getDataType().getFields()) {
-                WritableMap heartRateMap = Arguments.createMap();
-                heartRateMap.putString("startDate", dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
-                heartRateMap.putString("endDate", dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
-                heartRateMap.putDouble("quantity", dp.getValue(field).asFloat());
-                map.pushMap(heartRateMap);
+                if(index == 0) {
+                    ++index;
+                    heartRateMap.putString("startDate", dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+                    heartRateMap.putString("endDate", dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+                    heartRateMap.putDouble("quantity", dp.getValue(field).asFloat());
+                    map.pushMap(heartRateMap);
+                }
             }
         }
     }
